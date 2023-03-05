c450d7170cbdf7479b7f994be7fa25733cf904b2
Lawrence Forman <me@merklejerk.com>
2023-03-05 01:47:09 -0700

    moar

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,47 @@
           <input type="number" v-model="ethValue" class="bg-transparent border-transparent" />
           <span>wei</span>
         </div>
-        <button @click="handleExecute()" class="bg-yellow-400 rounded-full py-2 px-5 font-bold text-slate-700" :disabled="!runtimeCode">
+          <svg v-if="isExecuting" width="44" height="44" viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" stroke="#ffc626">
+            <g fill="none" fill-rule="evenodd" stroke-width="3">
+                <circle cx="22" cy="22" r="1">
+                    <animate attributeName="r"
+                        begin="0s" dur="1.8s"
+                        values="1; 20"
+                        calcMode="spline"
+                        keyTimes="0; 1"
+                        keySplines="0.165, 0.84, 0.44, 1"
+                        repeatCount="indefinite" />
+                    <animate attributeName="stroke-opacity"
+                        begin="0s" dur="1.8s"
+                        values="1; 0"
+                        calcMode="spline"
+                        keyTimes="0; 1"
+                        keySplines="0.3, 0.61, 0.355, 1"
+                        repeatCount="indefinite" />
+                </circle>
+                <circle cx="22" cy="22" r="1">
+                    <animate attributeName="r"
+                        begin="-0.9s" dur="1.8s"
+                        values="1; 20"
+                        calcMode="spline"
+                        keyTimes="0; 1"
+                        keySplines="0.165, 0.84, 0.44, 1"
+                        repeatCount="indefinite" />
+                    <animate attributeName="stroke-opacity"
+                        begin="-0.9s" dur="1.8s"
+                        values="1; 0"
+                        calcMode="spline"
+                        keyTimes="0; 1"
+                        keySplines="0.3, 0.61, 0.355, 1"
+                        repeatCount="indefinite" />
+                </circle>
+            </g>
+        </svg>
+        <button
+          v-else
+          @click="handleExecute()"
+          class="bg-yellow-400 rounded-full py-2 px-5 font-bold text-slate-700"
+          :disabled="!runtimeCode || isExecuting">
           Execute
         </button>
       </div>
